3ec08a55faa355692abab2ab11f5633df7343009
Isaque_Witt <167255023+IdSW26@users.noreply.github.com>
2025-02-20 12:12:52 -0300

    Add files via upload

---
+++ primeiro.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main() {
+
+    int A;
+    int B;
+    int AB;
+    int AxB;
+
+    A = 10;
+    B = 23;
+    AB = A + B;
+    AxB = A * B;
+    
+    printf("a vale: %d \n", A);
+    printf("b vale: %d \n", B);
+    printf("a + b vale: %d \n", AB);
+    printf("a * b vale: %d", AxB);
+    
+    return 0;
+}
+
